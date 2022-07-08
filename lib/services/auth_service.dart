be7b2bfba826609961c79474879b5190bc51e272
rafael <rafael.silva@tecnorisk.com.br>
2022-07-08 17:46:40 -0300

    update

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -7,8 +7,6 @@ import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
-  final _auth = FirebaseAuth.instance;
-
   handleAuthState() {
     // FirebaseAuth.instance.signOut();
     return StreamBuilder(
@@ -44,11 +42,17 @@ class AuthService {
   }
 
   signInWithFacebook() async {
-    final LoginResult loginResult = await FacebookAuth.instance.login();
+    try {
+      final LoginResult loginResult = await FacebookAuth.instance.login();
 
-    final OAuthCredential facebookAuthCredential =
-        FacebookAuthProvider.credential(loginResult.accessToken!.token);
-    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+      final OAuthCredential facebookAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.token);
+      return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+    } on PlatformException catch (e) {
+      print('error caught: $e');
+    } catch (e) {
+      print('error caught: $e');
+    }
   }
 
   signOut() {
---
+++ lib/services/user_verify.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VerifyUser extends StatefulWidget {
+  const VerifyUser({Key? key}) : super(key: key);
+
+  @override
+  State<VerifyUser> createState() => _VerifyUserState();
+}
+
+class _VerifyUserState extends State<VerifyUser> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
